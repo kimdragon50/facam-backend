3a01e120c8d87e956eac0c47e0abfc3a0c57be8f
김용만 <kym@gim-yongmans-MacBook-Pro.local>
2021-05-03 15:44:29 +0900

    kym

--- src/main/java/com/dursuneryilmaz/employeemanagement/config/SwaggerConfig.java
+++ src/main/java/com/dursuneryilmaz/employeemanagement/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig  {
     @SuppressWarnings("desprecation")
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
