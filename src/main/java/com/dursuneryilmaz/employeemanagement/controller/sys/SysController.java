ab6310a28aef1d49301017571c98d4ee631abe9b
김용만 <kym@gim-yongmans-MacBook-Pro.local>
2021-05-04 17:54:53 +0900

    kym

--- src/main/java/com/dursuneryilmaz/employeemanagement/controller/sys/SysController.java
+++ src/main/java/com/dursuneryilmaz/employeemanagement/controller/sys/SysController.java
@@ -1,5 +1,6 @@
 package com.dursuneryilmaz.employeemanagement.controller.sys;
 
+import org.aspectj.bridge.MessageUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sys")
 public class SysController {
 
+    private MessageUtil logger;
+
     // healthz check
     @GetMapping("/healthz")
     public String healthz() {
+        logger.debug("########################healthz");
         return "UP";
     }
 
